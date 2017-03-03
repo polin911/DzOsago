3d9374235591be289a2fda464aacbd0220417443
polin911 <polin911@mail.ru>
2017-03-03 10:53:26 +0300

    Initial Commit

---
+++ DzOsago/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DzOsago
+//
+//  Created by Polina on 03.03.17.
+//  Copyright © 2017 Polina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
